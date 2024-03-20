99fddecfa485bbfe42ed359e8080535058df6020
Yaroslav <yaroslav.danilenko.0996@gmail.com>
2024-03-20 20:56:29 +0300

    123

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,27 @@
+// module.exports = {
+//   apps: [
+//     {
+//       name: 'index',
+//       script: 'index.cjs',
+//       exec_mode: 'fork',
+//       interpreter: 'node',
+//       interpreter_args: '-r esm',
+//       restart_delay: 3000,
+//       max_restarts: 10,
+//     },
+//   ],
+// };
+const os = require('os');
 module.exports = {
-  apps: [
-    {
-      name: 'index',
-      script: 'index.cjs',
-      exec_mode: 'fork',
-      interpreter: 'node',
-      interpreter_args: '-r esm',
-      restart_delay: 3000,
-      max_restarts: 10,
-    },
-  ],
-};
+    apps: [{
+        port        : 3000,
+        name        : "any",
+        script      : "./index.cjs", // 👈 CommonJS
+        watch       : true,           
+        instances   : os.cpus().length,
+        exec_mode   : 'fork',         
+        env: {
+            NODE_ENV: "production",
+        }
+    }]
+}
